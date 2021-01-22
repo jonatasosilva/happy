3139c5d99c96353b5c0d91157180b55b69a78a6f
Jonatas Silva <54430451+jonatasosilva@users.noreply.github.com>
2021-01-22 02:15:27 -0300

    fix gambiarra

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image) {
         return {
             id: image.id,
-            url: `https://happy.jonatasosilva.dev/api/uploads/${image.path}`
+            url: `http://localhost:3334/uploads/${image.path}`
         };
     },
 
--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://happy.jonatasosilva.dev/api'
+    baseURL: 'http://localhost:3334'
 });
 
 export default api;
